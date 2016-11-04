e64d16d8ec87c9021bd1c7f1bf7022ae20f7090b
Turupawn <ahmed.hn.43@gmail.com>
2016-11-04 17:45:46 -0600

    agregando funcion /enviar

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ app.set('view engine', 'ejs')
 //Sqlite3 - BDD
 var sqlite3 = require('sqlite3').verbose()
 
+app.get('/enviar', function(req, res) {
+
+  var db = new sqlite3.Database("bdd.sqlite3")
+  db.serialize(function() {
+
+    var stmt = db.prepare("INSERT INTO mensajes VALUES (?,?)")
+    stmt.run(req.query.usuario,req.query.mensaje)
+    stmt.finalize()
+
+    var mensajes = []
+    db.all("select * from mensajes;", function(err, rows)
+    {  
+      rows.forEach(function (row) {  
+        mensajes.push([row.usuario, row.mensaje])
+      })
+      res.render('index', { mensajes: mensajes })
+    });
+
+  });
+  db.close();
+})
 
 
 app.get('/', function(req, res) {
